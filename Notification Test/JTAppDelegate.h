929ce0ace053fc53c442a9bed8dab8cef62a958a
James Topham <apple@Apples-MacBook-Pro.local>
2014-07-01 13:31:11 +0100

    Initial Commit

---
+++ Notification Test/JTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JTAppDelegate.h
+//  Notification Test
+//
+//  Created by James Topham on 01/07/2014.
+//  Copyright (c) 2014 James Topham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Notification Test/JTViewController.h
@@ -0,0 +1,13 @@
+//
+//  JTViewController.h
+//  Notification Test
+//
+//  Created by James Topham on 01/07/2014.
+//  Copyright (c) 2014 James Topham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JTViewController : UIViewController
+
+@end
